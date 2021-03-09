52056417c397b5f8e201400acd0603d1b5b90fec
roaldiopi <75036913+roaldiopi@users.noreply.github.com>
2021-03-09 18:05:43 +0200

    Update vk.py

--- api/vk.py
+++ api/vk.py
@@ -606,9 +606,3 @@ class Wall(VkApi):
             "wall.search", kwargs
         )
         return search
-
-    # def delete_comment(self, comment_id, owner_id=""):
-
-
-token = get_token(7779574, "wall,status", "+37362159808", "andrei2006telus")
-print(token)
